07b55eebfb491091c36b37debb26c56d0598dfad
Tommi Hirvonen <tommi.hirvonen@student.tut.fi>
2015-03-10 20:19:40 +0200

    Added about_dialog.java and frame_info_dialog.xml

---
+++ app/src/main/java/com/tommihirvonen/filmphotonotes/about_dialog.java
@@ -0,0 +1,12 @@
+package com.tommihirvonen.filmphotonotes;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Tommi on 10.3.2015.
+ */
+
