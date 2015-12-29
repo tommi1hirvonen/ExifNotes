c9c58e7e71aea28140ecbd39a59e413abefd47e4
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2015-12-29 20:36:52 +0200

    Action bar now works properly with Androids older than Lollipop.

--- app/src/main/java/com/tommihirvonen/filmphotonotes/PreferenceActivity.java
+++ app/src/main/java/com/tommihirvonen/filmphotonotes/PreferenceActivity.java
@@ -2,42 +2,97 @@ package com.tommihirvonen.filmphotonotes;
 
 
 
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.AppCompatCheckBox;
+import android.support.v7.widget.AppCompatCheckedTextView;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.AppCompatRadioButton;
+import android.support.v7.widget.AppCompatSpinner;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 
 // Copyright 2015
 // Tommi Hirvonen
-public class PreferenceActivity extends android.preference.PreferenceActivity {
 
+public class PreferenceActivity extends android.preference.PreferenceActivity {
 
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        // This is a way to get the action bar in Preferences
-        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
-        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.preference_toolbar , root, false);
-        root.addView(bar, 0); // insert at top
-        bar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
+        // This is a way to get the action bar in Preferences.
+        // It will be done only on Androids > 5.0.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+            Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.preference_toolbar , root, false);
+            root.addView(bar, 0); // insert at top
+            bar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             getWindow().setStatusBarColor( ContextCompat.getColor(this, R.color.secondary_color) );
         }
+        else {
 
+            // This is a legacy implementation. All this is needed in order to make
+            // the action bar title and icon appear in white. WTF!?
+            setContentView(R.layout.activity_settings_legacy);
+            Toolbar actionbar = (Toolbar) findViewById(R.id.actionbar);
+            actionbar.setTitle(R.string.Preferences);
+
+            actionbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
+            actionbar.setNavigationIcon(ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_mtrl_am_alpha));
+            actionbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+            getFragmentManager().beginTransaction().add(R.id.rel_layout, new PreferenceFragment()).commit();
+        }
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        // Allow super to try and create a view first
+        final View result = super.onCreateView(name, context, attrs);
+        if (result != null) {
+            return result;
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            // If we're running pre-L, we need to 'inject' our tint aware Views in place of the
+            // standard framework versions
+            switch (name) {
+                case "EditText":
+                    return new AppCompatEditText(this, attrs);
+                case "Spinner":
+                    return new AppCompatSpinner(this, attrs);
+                case "CheckBox":
+                    return new AppCompatCheckBox(this, attrs);
+                case "RadioButton":
+                    return new AppCompatRadioButton(this, attrs);
+                case "CheckedTextView":
+                    return new AppCompatCheckedTextView(this, attrs);
+            }
+        }
+
+        return null;
     }
 
 
