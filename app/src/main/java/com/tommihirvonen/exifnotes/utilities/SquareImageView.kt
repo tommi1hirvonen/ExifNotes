a6525a61720433fd04d14bd73aa40151656c246d
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2020-12-20 13:41:42 +0200

    Updated libraries

--- app/src/main/java/com/tommihirvonen/exifnotes/utilities/SquareImageView.kt
+++ app/src/main/java/com/tommihirvonen/exifnotes/utilities/SquareImageView.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.widget.AppCompatImageView
 /**
  * Custom ImageView class, that forces the view to be square.
  */
-class SquareImageView(context: Context?, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
+class SquareImageView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         // Set the view height to be the same as the measured width.
