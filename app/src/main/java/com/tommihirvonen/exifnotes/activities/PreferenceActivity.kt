f70cc6be810f5b301017a15d24d1cda385c0eb73
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2022-07-25 17:24:48 +0300

    PreferenceActivity Material3 theming

--- app/src/main/java/com/tommihirvonen/exifnotes/activities/PreferenceActivity.kt
+++ app/src/main/java/com/tommihirvonen/exifnotes/activities/PreferenceActivity.kt
@@ -18,12 +18,10 @@
 
 package com.tommihirvonen.exifnotes.activities
 
-import android.content.SharedPreferences
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceManager
+import com.google.android.material.appbar.MaterialToolbar
 import com.tommihirvonen.exifnotes.R
 import com.tommihirvonen.exifnotes.fragments.PreferenceFragment
 import com.tommihirvonen.exifnotes.utilities.*
@@ -32,7 +30,7 @@ import com.tommihirvonen.exifnotes.utilities.*
  * PreferenceActivity contains the PreferenceFragment for editing the app's settings
  * and preferences.
  */
-class PreferenceActivity : AppCompatActivity(), OnSharedPreferenceChangeListener {
+class PreferenceActivity : AppCompatActivity() {
 
     companion object {
         /**
@@ -58,24 +56,20 @@ class PreferenceActivity : AppCompatActivity(), OnSharedPreferenceChangeListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         overridePendingTransition(R.anim.enter_from_right, R.anim.hold)
-
-        if (isAppThemeDark) {
-            setTheme(R.style.AppTheme_Dark)
-        }
-
         super.onCreate(savedInstanceState)
 
         // If the activity was recreated, get the saved result code
         savedInstanceState?.let { resultCode = it.getInt(ExtraKeys.RESULT_CODE) }
 
         setContentView(R.layout.activity_settings)
-        setUiColor(true)
-        supportActionBar?.title = resources.getString(R.string.Preferences)
-        val prefs = PreferenceManager.getDefaultSharedPreferences(baseContext)
-        prefs.registerOnSharedPreferenceChangeListener(this)
+        
+        val topAppBar = findViewById<MaterialToolbar>(R.id.top_app_bar)
+        topAppBar.setNavigationOnClickListener {
+            finish()
+        }
 
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().add(R.id.rel_layout, PreferenceFragment()).commit()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_layout, PreferenceFragment()).commit()
         }
     }
 
@@ -88,11 +82,6 @@ class PreferenceActivity : AppCompatActivity(), OnSharedPreferenceChangeListener
         // Save the result code so that it can be set for this activity's result when recreated
         super.onSaveInstanceState(outState)
         outState.putInt(ExtraKeys.RESULT_CODE, resultCode)
-    }
-
-    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        setStatusBarColor(secondaryUiColor)
-        setSupportActionBarColor(primaryUiColor)
     }
 
     override fun finish() {
