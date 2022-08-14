c4b42285fb230f844ead4d6cfec29252e24838ab
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2022-08-14 15:49:03 +0300

    Implemented Material 3 styling to filter edit dialog

--- app/src/main/java/com/tommihirvonen/exifnotes/dialogs/EditFilterDialog.kt
+++ app/src/main/java/com/tommihirvonen/exifnotes/dialogs/EditFilterDialog.kt
@@ -25,7 +25,7 @@ import android.view.WindowManager
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.tommihirvonen.exifnotes.R
 import com.tommihirvonen.exifnotes.databinding.DialogFilterBinding
 import com.tommihirvonen.exifnotes.datastructures.Filter
@@ -47,7 +47,7 @@ class EditFilterDialog : DialogFragment() {
     override fun onCreateDialog(SavedInstanceState: Bundle?): Dialog {
         val layoutInflater = requireActivity().layoutInflater
         val binding = DialogFilterBinding.inflate(layoutInflater)
-        val builder = AlertDialog.Builder(requireActivity())
+        val builder = MaterialAlertDialogBuilder(requireActivity())
         val title = requireArguments().getString(ExtraKeys.TITLE)
         builder.setTitle(title)
         builder.setView(binding.root)
