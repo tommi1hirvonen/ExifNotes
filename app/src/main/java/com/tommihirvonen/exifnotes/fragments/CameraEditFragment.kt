851d1003f3a3377a8deba77514b19f91ae1c4277
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2022-08-24 17:16:10 +0300

    Implemented CameraEditViewModel

--- app/src/main/java/com/tommihirvonen/exifnotes/fragments/CameraEditFragment.kt
+++ app/src/main/java/com/tommihirvonen/exifnotes/fragments/CameraEditFragment.kt
@@ -23,36 +23,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
 import androidx.activity.addCallback
 import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import androidx.lifecycle.ViewModelProvider
 import androidx.transition.*
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textfield.MaterialAutoCompleteTextView
 import com.tommihirvonen.exifnotes.R
 import com.tommihirvonen.exifnotes.databinding.FragmentCameraEditBinding
 import com.tommihirvonen.exifnotes.datastructures.Camera
-import com.tommihirvonen.exifnotes.datastructures.Increment
 import com.tommihirvonen.exifnotes.datastructures.Lens
-import com.tommihirvonen.exifnotes.datastructures.PartialIncrement
 import com.tommihirvonen.exifnotes.utilities.*
+import com.tommihirvonen.exifnotes.viewmodels.CameraEditViewModel
+import com.tommihirvonen.exifnotes.viewmodels.CameraEditViewModelFactory
 
 /**
  * Dialog to edit Camera's information
  */
 class CameraEditFragment : Fragment() {
 
-    private lateinit var binding: FragmentCameraEditBinding
-
     private val camera by lazy { requireArguments().getParcelable(ExtraKeys.CAMERA) ?: Camera() }
-
-    private val newCamera by lazy { camera.copy() }
-
-    private lateinit var shutterValueOptions: Array<String>
+    private val model by lazy {
+        val factory = CameraEditViewModelFactory(requireActivity().application, camera.copy())
+        ViewModelProvider(this, factory)[CameraEditViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,110 +59,13 @@ class CameraEditFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = FragmentCameraEditBinding.inflate(inflater)
+        val binding = FragmentCameraEditBinding.inflate(inflater)
 
         val transitionName = requireArguments().getString(ExtraKeys.TRANSITION_NAME)
         binding.root.transitionName = transitionName
         binding.topAppBar.title = requireArguments().getString(ExtraKeys.TITLE)
-
-        // EDIT TEXT FIELDS
-        binding.makeEditText.setText(camera.make)
-        binding.modelEditText.setText(camera.model)
-        binding.serialNumberEditText.setText(camera.serialNumber)
-
-        // SHUTTER SPEED INCREMENTS
-        val minShutterMenu = binding.minShutterMenu.editText as MaterialAutoCompleteTextView
-        val maxShutterMenu = binding.maxShutterMenu.editText as MaterialAutoCompleteTextView
-
-        val shutterIncrementValues = resources.getStringArray(R.array.StopIncrements)
-        val shutterIncrementMenu =
-            binding.shutterSpeedIncrementsMenu.editText as MaterialAutoCompleteTextView
-        shutterIncrementMenu.setSimpleItems(shutterIncrementValues)
-        try {
-            val text = shutterIncrementValues[camera.shutterIncrements.ordinal]
-            shutterIncrementMenu.setText(text, false)
-        } catch (e: ArrayIndexOutOfBoundsException) {
-            e.printStackTrace()
-        }
-        shutterIncrementMenu.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            newCamera.shutterIncrements = Increment.from(position)
-        }
-
-        shutterValueOptions = getShutterValueOptions()
-
-        shutterIncrementMenu.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            newCamera.shutterIncrements = Increment.from(position)
-            shutterValueOptions = getShutterValueOptions()
-            minShutterMenu.setSimpleItems(shutterValueOptions)
-            maxShutterMenu.setSimpleItems(shutterValueOptions)
-            val minFound = shutterValueOptions.contains(newCamera.minShutter)
-            val maxFound = shutterValueOptions.contains(newCamera.maxShutter)
-            if (!minFound || !maxFound) {
-                newCamera.minShutter = null
-                newCamera.maxShutter = null
-                minShutterMenu.setText(null, false)
-                maxShutterMenu.setText(null, false)
-            }
-        }
-
-        binding.clearShutterRange.setOnClickListener {
-            newCamera.minShutter = null
-            newCamera.maxShutter = null
-            minShutterMenu.setText(null, false)
-            maxShutterMenu.setText(null, false)
-        }
-        minShutterMenu.setSimpleItems(shutterValueOptions)
-        maxShutterMenu.setSimpleItems(shutterValueOptions)
-        minShutterMenu.setText(newCamera.minShutter, false)
-        maxShutterMenu.setText(newCamera.maxShutter, false)
-
-        minShutterMenu.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            if (position > 0) {
-                newCamera.minShutter = shutterValueOptions[position]
-            } else {
-                newCamera.minShutter = null
-                minShutterMenu.setText(null, false)
-            }
-        }
-        maxShutterMenu.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            if (position > 0) {
-                newCamera.maxShutter = shutterValueOptions[position]
-            } else {
-                newCamera.maxShutter = null
-                maxShutterMenu.setText(null, false)
-            }
-        }
-
-        // The end icon of TextInputLayout can be used to toggle the menu open/closed.
-        // However in that case, the AutoCompleteTextView onClick method is not called.
-        // By setting the endIconOnClickListener to null onClick events are propagated
-        // to AutoCompleteTextView. This way we can force the preselection of the current item.
-        binding.minShutterMenu.setEndIconOnClickListener(null)
-        minShutterMenu.setOnClickListener {
-            val currentIndex = shutterValueOptions.indexOf(minShutterMenu.text.toString())
-            if (currentIndex >= 0) minShutterMenu.listSelection = currentIndex
-        }
-        binding.maxShutterMenu.setEndIconOnClickListener(null)
-        maxShutterMenu.setOnClickListener {
-            val currentIndex = shutterValueOptions.indexOf(maxShutterMenu.text.toString())
-            if (currentIndex >= 0) maxShutterMenu.listSelection = currentIndex
-        }
-
-
-        // EXPOSURE COMPENSATION INCREMENTS
-        val exposureCompIncrementValues = resources.getStringArray(R.array.ExposureCompIncrements)
-        val exposureCompIncrementMenu =
-            binding.expCompIncrementsMenu.editText as MaterialAutoCompleteTextView
-        exposureCompIncrementMenu.setSimpleItems(exposureCompIncrementValues)
-        try {
-            val text = exposureCompIncrementValues[camera.exposureCompIncrements.ordinal]
-            exposureCompIncrementMenu.setText(text, false)
-        } catch (e: ArrayIndexOutOfBoundsException) {
-            e.printStackTrace()
-        }
-        exposureCompIncrementMenu.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            newCamera.exposureCompIncrements = PartialIncrement.from(position)
-        }
+        binding.topAppBar.setNavigationOnClickListener { navigateBack() }
+        binding.viewmodel = model.observable
 
         // FIXED LENS
         binding.fixedLensHelp.setOnClickListener {
@@ -174,65 +74,23 @@ class CameraEditFragment : Fragment() {
                 setPositiveButton(R.string.Close) { _: DialogInterface, _: Int -> }
             }.create().show()
         }
-        newCamera.lens?.let {
-            binding.fixedLensLayout.text = resources.getString(R.string.ClickToEdit)
-            binding.lensClear.visibility = View.VISIBLE
-        } ?: run {
-            binding.fixedLensLayout.text = resources.getString(R.string.ClickToSet)
-            binding.lensClear.visibility = View.INVISIBLE
-        }
         binding.fixedLensLayout.setOnClickListener {
-            showFixedLensFragment()
+            showFixedLensFragment(binding.fixedLensLayout)
         }
-        binding.lensClear.setOnClickListener {
-            newCamera.lens = null
-            binding.fixedLensLayout.text = resources.getString(R.string.ClickToSet)
-            binding.lensClear.visibility = View.INVISIBLE
-        }
-
-        binding.topAppBar.setNavigationOnClickListener { navigateBack() }
-
         binding.positiveButton.setOnClickListener {
-            val make = binding.makeEditText.text.toString()
-            val model = binding.modelEditText.text.toString()
-            val serialNumber = binding.serialNumberEditText.text.toString()
-
-            val nameValidation = { _: Camera ->
-                (make.isNotEmpty() && model.isNotEmpty()) to
-                        resources.getString(R.string.MakeAndOrModelIsEmpty)
-            }
-            val shutterRangeValidation1 = { c: Camera ->
-                (c.minShutter == null && c.maxShutter == null
-                        || c.minShutter != null && c.maxShutter != null) to
-                        resources.getString(R.string.NoMinOrMaxShutter)
-            }
-            val shutterRangeValidation2 = { c: Camera ->
-                if (c.minShutter == null && c.maxShutter == null) {
-                    true to ""
-                } else {
-                    val minIndex = shutterValueOptions.indexOf(c.minShutter)
-                    val maxIndex = shutterValueOptions.indexOf(c.maxShutter)
-                    (maxIndex >= minIndex) to resources.getString(R.string.MinShutterSpeedGreaterThanMax)
-                }
-            }
-            val (validationResult, validationMessage) = newCamera.validate(
-                nameValidation, shutterRangeValidation1, shutterRangeValidation2)
-
-            if (validationResult) {
-                camera.make = make
-                camera.model = model
-                camera.serialNumber = serialNumber
-                camera.shutterIncrements = newCamera.shutterIncrements
-                camera.minShutter = newCamera.minShutter
-                camera.maxShutter = newCamera.maxShutter
-                camera.exposureCompIncrements = newCamera.exposureCompIncrements
-                camera.lens = newCamera.lens
+            if (model.validate()) {
+                camera.make = model.camera.make
+                camera.model = model.camera.model
+                camera.serialNumber = model.camera.serialNumber
+                camera.shutterIncrements = model.camera.shutterIncrements
+                camera.minShutter = model.camera.minShutter
+                camera.maxShutter = model.camera.maxShutter
+                camera.exposureCompIncrements = model.camera.exposureCompIncrements
+                camera.lens = model.camera.lens
                 val bundle = Bundle()
                 bundle.putParcelable(ExtraKeys.CAMERA, camera)
                 setFragmentResult("CameraEditFragment", bundle)
                 navigateBack()
-            } else {
-                binding.root.snackbar(validationMessage)
             }
         }
         return binding.root
@@ -250,13 +108,7 @@ class CameraEditFragment : Fragment() {
     private fun navigateBack() =
         requireParentFragment().childFragmentManager.popBackStack()
 
-    private fun getShutterValueOptions() = when (newCamera.shutterIncrements) {
-        Increment.THIRD -> requireActivity().resources.getStringArray(R.array.ShutterValuesThird)
-        Increment.HALF -> requireActivity().resources.getStringArray(R.array.ShutterValuesHalf)
-        Increment.FULL -> requireActivity().resources.getStringArray(R.array.ShutterValuesFull)
-    }.reversedArray()
-
-    private fun showFixedLensFragment() {
+    private fun showFixedLensFragment(sharedElement: View) {
         val sharedElementTransition = TransitionSet()
             .addTransition(ChangeBounds())
             .addTransition(ChangeTransform())
@@ -269,11 +121,10 @@ class CameraEditFragment : Fragment() {
         }
         val arguments = Bundle()
         arguments.putBoolean(ExtraKeys.FIXED_LENS, true)
-        newCamera.lens?.let {
+        model.camera.lens?.let {
             arguments.putParcelable(ExtraKeys.LENS, it)
         }
         arguments.putString(ExtraKeys.TITLE, resources.getString(R.string.SetFixedLens))
-        val sharedElement = binding.fixedLensLayout
         arguments.putString(ExtraKeys.TRANSITION_NAME, sharedElement.transitionName)
         fragment.arguments = arguments
 
@@ -288,9 +139,7 @@ class CameraEditFragment : Fragment() {
         fragment.setFragmentResultListener("LensEditFragment") { _, bundle ->
             val lens: Lens = bundle.getParcelable(ExtraKeys.LENS)
                 ?: return@setFragmentResultListener
-            newCamera.lens = lens
-            binding.fixedLensLayout.text = resources.getString(R.string.ClickToEdit)
-            binding.lensClear.visibility = View.VISIBLE
+            model.observable.setLens(lens)
         }
     }
 
---
+++ app/src/main/java/com/tommihirvonen/exifnotes/viewmodels/CameraEditViewModel.kt
@@ -0,0 +1,257 @@
+/*
+ * Exif Notes
+ * Copyright (C) 2022  Tommi Hirvonen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.tommihirvonen.exifnotes.viewmodels
+
+import android.app.Application
+import android.view.View
+import android.widget.AdapterView
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
+import com.tommihirvonen.exifnotes.BR
+import com.tommihirvonen.exifnotes.R
+import com.tommihirvonen.exifnotes.datastructures.Camera
+import com.tommihirvonen.exifnotes.datastructures.Increment
+import com.tommihirvonen.exifnotes.datastructures.Lens
+import com.tommihirvonen.exifnotes.datastructures.PartialIncrement
+import com.tommihirvonen.exifnotes.utilities.validate
+
+class CameraEditViewModel(application: Application, val camera: Camera)
+    : AndroidViewModel(application) {
+
+    val context get() = getApplication<Application>()
+
+    val observable = Observable()
+
+    fun validate(): Boolean =
+        camera.validate(makeValidation, modelValidation, ::shutterRangeValidation)
+
+    private val makeValidation = { c: Camera ->
+        if (c.make.isNullOrEmpty()) {
+            observable.makeError = context.getString(R.string.Required)
+            false
+        } else {
+            true
+        }
+    }
+
+    private val modelValidation = { c: Camera ->
+        if (c.model.isNullOrEmpty()) {
+            observable.modelError = context.getString(R.string.Required)
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun shutterRangeValidation(camera: Camera): Boolean {
+        // Both ends of the shutter range must be provided.
+        if (camera.minShutter == null && camera.maxShutter != null ||
+            camera.minShutter != null && camera.maxShutter == null) {
+            observable.shutterRangeError = context.getString(R.string.NoMinOrMaxShutter)
+            return false
+        }
+        val minIndex = observable.shutterValueOptions.indexOf(camera.minShutter)
+        val maxIndex = observable.shutterValueOptions.indexOf(camera.maxShutter)
+        if (minIndex > maxIndex) {
+            observable.shutterRangeError = context.getString(R.string.MinShutterSpeedGreaterThanMax)
+            return false
+        }
+        return true
+    }
+
+    inner class Observable : BaseObservable() {
+        @Bindable
+        fun getMake() = camera.make
+        fun setMake(value: String?) {
+            if (camera.make != value) {
+                camera.make = value
+                notifyPropertyChanged(BR.make)
+            }
+        }
+
+        @Bindable
+        var makeError: String? = null
+            set(value) {
+                   field = value?.ifEmpty { null }
+                notifyPropertyChanged(BR.makeError)
+            }
+
+        @Bindable
+        fun getModel() = camera.model
+        fun setModel(value: String?) {
+            if (camera.model != value) {
+                camera.model = value
+                notifyPropertyChanged(BR.model)
+            }
+        }
+
+        @Bindable
+        var modelError: String? = null
+            set(value) {
+                field = value?.ifEmpty { null }
+                notifyPropertyChanged(BR.modelError)
+            }
+
+        @Bindable
+        fun getSerialNumber() = camera.serialNumber
+        fun setSerialNumber(value: String?) {
+            if (camera.serialNumber != value) {
+                camera.serialNumber = value
+                notifyPropertyChanged(BR.serialNumber)
+            }
+        }
+
+        @Bindable
+        fun getMinShutter() = camera.minShutter
+        private fun setMinShutter(value: String?) {
+            if (camera.minShutter != value) {
+                camera.minShutter = value
+                notifyPropertyChanged(BR.minShutter)
+                shutterRangeError = null
+            }
+        }
+
+        @Bindable
+        fun getMaxShutter() = camera.maxShutter
+        private fun setMaxShutter(value: String?) {
+            if (camera.maxShutter != value) {
+                camera.maxShutter = value
+                notifyPropertyChanged(BR.maxShutter)
+                shutterRangeError = null
+            }
+        }
+
+        @Bindable
+        var shutterRangeError: String? = null
+            set(value) {
+                field = value?.ifEmpty { null }
+                notifyPropertyChanged(BR.shutterRangeError)
+            }
+
+        @get:Bindable
+        var shutterValueOptions: Array<String> = shutterValues
+
+        val shutterIncrement: String = context.resources
+            .getStringArray(R.array.StopIncrements)[camera.shutterIncrements.ordinal]
+
+        val shutterIncrementOnItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                camera.shutterIncrements = Increment.from(position)
+                shutterValueOptions = shutterValues
+                notifyPropertyChanged(BR.shutterValueOptions)
+                val minFound = shutterValueOptions.contains(camera.minShutter)
+                val maxFound = shutterValueOptions.contains(camera.maxShutter)
+                // If either one wasn't found in the new values array, null them.
+                if (!minFound || !maxFound) {
+                    setMinShutter(null)
+                    setMaxShutter(null)
+                }
+            }
+
+        val minShutterOnItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            // Handle empty item selection (first in list)
+            if (position > 0) {
+                setMinShutter(shutterValueOptions[position])
+            } else {
+                setMinShutter(null)
+            }
+        }
+
+        val maxShutterOnItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            // Handle empty item selection (first in list)
+            if (position > 0) {
+                setMaxShutter(shutterValueOptions[position])
+            } else {
+                setMaxShutter(null)
+            }
+        }
+
+        // Handle list item preselection and scrolling when opened.
+        val minShutterOnClickListener = View.OnClickListener { view ->
+            val index = shutterValueOptions.indexOf(camera.minShutter)
+            if (index >= 0) (view as MaterialAutoCompleteTextView).listSelection = index
+        }
+
+        // Handle list item preselection and scrolling when opened.
+        val maxShutterOnClickListener = View.OnClickListener { view ->
+            val index = shutterValueOptions.indexOf(camera.maxShutter)
+            if (index >= 0) (view as MaterialAutoCompleteTextView).listSelection = index
+        }
+
+        fun clearShutterRange() {
+            setMinShutter(null)
+            setMaxShutter(null)
+        }
+
+        private val shutterValues get() = when (camera.shutterIncrements) {
+            Increment.THIRD -> context.resources.getStringArray(R.array.ShutterValuesThird)
+            Increment.HALF -> context.resources.getStringArray(R.array.ShutterValuesHalf)
+            Increment.FULL -> context.resources.getStringArray(R.array.ShutterValuesFull)
+        }.reversedArray()
+
+        val exposureCompensationIncrement: String = context.resources
+            .getStringArray(R.array.ExposureCompIncrements)[camera.exposureCompIncrements.ordinal]
+
+        val exposureCompensationIncrementOnItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                camera.exposureCompIncrements = PartialIncrement.from(position)
+            }
+
+        @get:Bindable
+        val fixedLensSummary: String get() = if (camera.lens == null) {
+            context.resources.getString(R.string.ClickToSet)
+        } else {
+            context.resources.getString(R.string.ClickToEdit)
+        }
+
+        @get:Bindable
+        val clearLensVisibility get() = if (camera.lens == null) {
+            View.INVISIBLE
+        } else {
+            View.VISIBLE
+        }
+
+        fun setLens(lens: Lens) {
+            camera.lens = lens
+            notifyPropertyChanged(BR.fixedLensSummary)
+            notifyPropertyChanged(BR.clearLensVisibility)
+        }
+
+        fun clearLens() {
+            camera.lens = null
+            notifyPropertyChanged(BR.fixedLensSummary)
+            notifyPropertyChanged(BR.clearLensVisibility)
+        }
+    }
+}
+
+class CameraEditViewModelFactory(private val application: Application, private val camera: Camera)
+    : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        @Suppress("UNCHECKED_CAST")
+        if (modelClass.isAssignableFrom(CameraEditViewModel::class.java)) {
+            return CameraEditViewModel(application, camera) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
