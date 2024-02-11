f0bffee4166ad16aecac6ea008ce158545c04a84
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-02-11 12:10:36 +0200

    [data] moved constants from Database.kt to separate files

---
+++ data/src/main/java/com/tommihirvonen/exifnotes/data/constants/ColumnConstants.kt
@@ -0,0 +1,82 @@
+/*
+ * Exif Notes
+ * Copyright (C) 2024  Tommi Hirvonen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.tommihirvonen.exifnotes.data.constants
+
+internal const val KEY_FRAME_ID = "frame_id"
+internal const val KEY_COUNT = "count"
+internal const val KEY_DATE = "date"
+internal const val KEY_SHUTTER = "shutter"
+internal const val KEY_APERTURE = "aperture"
+internal const val KEY_FRAME_NOTE = "frame_note"
+internal const val KEY_LOCATION = "location"
+internal const val KEY_FOCAL_LENGTH = "focal_length"
+internal const val KEY_EXPOSURE_COMP = "exposure_comp"
+internal const val KEY_NO_OF_EXPOSURES = "no_of_exposures"
+internal const val KEY_FLASH_USED = "flash_used"
+internal const val KEY_FLASH_POWER = "flash_power"
+internal const val KEY_FLASH_COMP = "flash_comp"
+internal const val KEY_FRAME_SIZE = "frame_size"
+internal const val KEY_METERING_MODE = "metering_mode"
+internal const val KEY_FORMATTED_ADDRESS = "formatted_address"
+internal const val KEY_PICTURE_FILENAME = "picture_filename"
+internal const val KEY_LIGHT_SOURCE = "light_source"
+
+internal const val KEY_LENS_ID = "lens_id"
+internal const val KEY_LENS_MAKE = "lens_make"
+internal const val KEY_LENS_MODEL = "lens_model"
+internal const val KEY_LENS_MAX_APERTURE = "lens_max_aperture"
+internal const val KEY_LENS_MIN_APERTURE = "lens_min_aperture"
+internal const val KEY_LENS_MAX_FOCAL_LENGTH = "lens_max_focal_length"
+internal const val KEY_LENS_MIN_FOCAL_LENGTH = "lens_min_focal_length"
+internal const val KEY_LENS_SERIAL_NO = "lens_serial_no"
+internal const val KEY_LENS_APERTURE_INCREMENTS = "aperture_increments"
+internal const val KEY_LENS_CUSTOM_APERTURE_VALUES = "custom_aperture_values"
+
+internal const val KEY_CAMERA_ID = "camera_id"
+internal const val KEY_CAMERA_MAKE = "camera_make"
+internal const val KEY_CAMERA_MODEL = "camera_model"
+internal const val KEY_CAMERA_FORMAT = "camera_format"
+internal const val KEY_CAMERA_MAX_SHUTTER = "camera_max_shutter"
+internal const val KEY_CAMERA_MIN_SHUTTER = "camera_min_shutter"
+internal const val KEY_CAMERA_SERIAL_NO = "camera_serial_no"
+internal const val KEY_CAMERA_SHUTTER_INCREMENTS = "shutter_increments"
+internal const val KEY_CAMERA_EXPOSURE_COMP_INCREMENTS = "exposure_comp_increments"
+
+internal const val KEY_ROLL_ID = "roll_id"
+internal const val KEY_ROLLNAME = "rollname"
+internal const val KEY_ROLL_DATE = "roll_date"
+internal const val KEY_ROLL_NOTE = "roll_note"
+internal const val KEY_ROLL_ISO = "roll_iso"
+internal const val KEY_ROLL_PUSH = "roll_push"
+internal const val KEY_ROLL_FORMAT = "roll_format"
+internal const val KEY_ROLL_ARCHIVED = "roll_archived"
+internal const val KEY_ROLL_UNLOADED = "roll_unloaded"
+internal const val KEY_ROLL_DEVELOPED = "roll_developed"
+
+internal const val KEY_FILTER_ID = "filter_id"
+internal const val KEY_FILTER_MAKE = "filter_make"
+internal const val KEY_FILTER_MODEL = "filter_model"
+
+internal const val KEY_FILM_STOCK_ID = "film_stock_id"
+internal const val KEY_FILM_MANUFACTURER_NAME = "film_manufacturer_name"
+internal const val KEY_FILM_STOCK_NAME = "film_stock_name"
+internal const val KEY_FILM_ISO = "film_iso"
+internal const val KEY_FILM_TYPE = "film_type"
+internal const val KEY_FILM_PROCESS = "film_process"
+internal const val KEY_FILM_IS_PREADDED = "film_is_preadded"
---
+++ data/src/main/java/com/tommihirvonen/exifnotes/data/constants/TableConstants.kt
@@ -0,0 +1,29 @@
+/*
+ * Exif Notes
+ * Copyright (C) 2024  Tommi Hirvonen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.tommihirvonen.exifnotes.data.constants
+
+internal const val TABLE_FRAMES = "frames"
+internal const val TABLE_LENSES = "lenses"
+internal const val TABLE_ROLLS = "rolls"
+internal const val TABLE_CAMERAS = "cameras"
+internal const val TABLE_LINK_CAMERA_LENS = "link_camera_lens"
+internal const val TABLE_FILTERS = "filters"
+internal const val TABLE_LINK_LENS_FILTER = "link_lens_filter"
+internal const val TABLE_LINK_FRAME_FILTER = "link_frame_filter"
+internal const val TABLE_FILM_STOCKS = "film_stocks"
