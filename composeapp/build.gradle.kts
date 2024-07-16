c72cd188581868a4665635e94eb4aa627bc5159f
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-07-16 12:14:25 +0300

    [composeapp] compose navigation support

--- composeapp/build.gradle.kts
+++ composeapp/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.jetbrains.kotlin.compose)
     alias(libs.plugins.jetbrains.kotlin.kapt)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
 }
 
 android {
@@ -104,7 +105,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.icons)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.jetbrains.kotlinx.serialization.json)
     implementation(libs.hilt)
     kapt(libs.hilt.compiler)
     testImplementation(libs.junit)
