8323e56744717201038230d95452367482ea7fd5
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-07-18 00:56:39 +0300

    [composeapp] license screen preview improvements

--- composeapp/src/main/java/com/tommihirvonen/exifnotes/screens/settings/ThirdPartyLicensesScreen.kt
+++ composeapp/src/main/java/com/tommihirvonen/exifnotes/screens/settings/ThirdPartyLicensesScreen.kt
@@ -36,8 +36,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.tommihirvonen.exifnotes.R
+
+@Preview
+@Composable
+private fun ThirdPartyLicensesScreenPreview() {
+    ThirdPartyLicensesScreen(onNavigateUp = {})
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
