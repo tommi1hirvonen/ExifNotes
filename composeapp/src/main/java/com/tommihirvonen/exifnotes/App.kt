c5edaa8c1fc7ecd2dd15f96a8c98c60caf20f5dd
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-07-18 13:27:30 +0300

    [composeapp] moved top app level composable to separate file

---
+++ composeapp/src/main/java/com/tommihirvonen/exifnotes/App.kt
@@ -0,0 +1,125 @@
+/*
+ * Exif Notes
+ * Copyright (C) 2024  Tommi Hirvonen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.tommihirvonen.exifnotes
+
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
+import com.tommihirvonen.exifnotes.screens.labeledit.LabelEditScreen
+import com.tommihirvonen.exifnotes.screens.labelslist.LabelsScreen
+import com.tommihirvonen.exifnotes.screens.main.MainScreen
+import com.tommihirvonen.exifnotes.screens.main.MainViewModel
+import com.tommihirvonen.exifnotes.screens.settings.LicenseScreen
+import com.tommihirvonen.exifnotes.screens.settings.SettingsScreen
+import com.tommihirvonen.exifnotes.screens.settings.SettingsViewModel
+import com.tommihirvonen.exifnotes.screens.settings.ThirdPartyLicensesScreen
+import com.tommihirvonen.exifnotes.theme.ExifNotesTheme
+import com.tommihirvonen.exifnotes.theme.ThemeViewModel
+import kotlinx.serialization.Serializable
+
+@Composable
+fun App() {
+    val themeModel = hiltViewModel<ThemeViewModel>()
+    ExifNotesTheme(themeModel) {
+        val navController = rememberNavController()
+        val rollsModel = hiltViewModel<MainViewModel>()
+        val settingsModel = hiltViewModel<SettingsViewModel>()
+        NavHost(
+            navController = navController,
+            enterTransition = { slideInHorizontally(initialOffsetX = { it }) },
+            exitTransition = { slideOutHorizontally() },
+            popEnterTransition = { slideInHorizontally() },
+            popExitTransition = { slideOutHorizontally(targetOffsetX = { it }) },
+            startDestination = Main
+        ) {
+            composable<Main> {
+                MainScreen(
+                    mainViewModel = rollsModel,
+                    onNavigateToMap = { /*TODO*/ },
+                    onNavigateToGear = { /*TODO*/ },
+                    onNavigateToLabels = { navController.navigate(route = Labels) },
+                    onNavigateToSettings = { navController.navigate(route = Settings) }
+                )
+            }
+            composable<Labels> {
+                LabelsScreen(
+                    mainViewModel = rollsModel,
+                    onNavigateUp = { navController.navigateUp() },
+                    onEditLabel = { label ->
+                        navController.navigate(route = LabelEdit(label?.id ?: -1))
+                    }
+                )
+            }
+            composable<Settings> {
+                SettingsScreen(
+                    themeViewModel = themeModel,
+                    settingsViewModel = settingsModel,
+                    mainViewModel = rollsModel,
+                    onNavigateUp = { navController.navigateUp() },
+                    onNavigateToLicense = { navController.navigate(route = License) },
+                    onNavigateToThirdPartyLicenses = {
+                        navController.navigate(route = ThirdPartyLicenses)
+                    }
+                )
+            }
+            composable<License> {
+                LicenseScreen(
+                    onNavigateUp = { navController.navigateUp() }
+                )
+            }
+            composable<ThirdPartyLicenses> {
+                ThirdPartyLicensesScreen(
+                    onNavigateUp = { navController.navigateUp() }
+                )
+            }
+            dialog<LabelEdit> { backStackEntry ->
+                val labelEdit = backStackEntry.toRoute<LabelEdit>()
+                LabelEditScreen(
+                    labelId = labelEdit.labelId,
+                    onDismiss = { navController.navigateUp() },
+                    mainViewModel = rollsModel
+                )
+            }
+        }
+    }
+}
+
+@Serializable
+private object Main
+
+@Serializable
+private object Labels
+
+@Serializable
+private data class LabelEdit(val labelId: Long)
+
+@Serializable
+private object Settings
+
+@Serializable
+private object License
+
+@Serializable
+private object ThirdPartyLicenses
--- composeapp/src/main/java/com/tommihirvonen/exifnotes/MainActivity.kt
+++ composeapp/src/main/java/com/tommihirvonen/exifnotes/MainActivity.kt
@@ -22,28 +22,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.dialog
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import com.tommihirvonen.exifnotes.di.pictures.ComplementaryPicturesManager
-import com.tommihirvonen.exifnotes.screens.labeledit.LabelEditScreen
-import com.tommihirvonen.exifnotes.screens.labelslist.LabelsScreen
-import com.tommihirvonen.exifnotes.screens.main.MainScreen
-import com.tommihirvonen.exifnotes.screens.main.MainViewModel
-import com.tommihirvonen.exifnotes.screens.settings.LicenseScreen
-import com.tommihirvonen.exifnotes.screens.settings.SettingsScreen
-import com.tommihirvonen.exifnotes.screens.settings.SettingsViewModel
-import com.tommihirvonen.exifnotes.screens.settings.ThirdPartyLicensesScreen
-import com.tommihirvonen.exifnotes.theme.ExifNotesTheme
-import com.tommihirvonen.exifnotes.theme.ThemeViewModel
 import com.tommihirvonen.exifnotes.util.purgeDirectory
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.serialization.Serializable
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -64,71 +45,7 @@ class MainActivity : ComponentActivity() {
         }
 
         enableEdgeToEdge()
-        setContent {
-            val themeModel = hiltViewModel<ThemeViewModel>()
-            ExifNotesTheme(themeModel) {
-                val navController = rememberNavController()
-                val rollsModel = hiltViewModel<MainViewModel>()
-                val settingsModel = hiltViewModel<SettingsViewModel>()
-                NavHost(
-                    navController = navController,
-                    enterTransition = { slideInHorizontally(initialOffsetX = { it }) },
-                    exitTransition = { slideOutHorizontally() },
-                    popEnterTransition = { slideInHorizontally() },
-                    popExitTransition = { slideOutHorizontally(targetOffsetX = { it }) },
-                    startDestination = Main
-                ) {
-                    composable<Main> {
-                        MainScreen(
-                            mainViewModel = rollsModel,
-                            onNavigateToMap = { /*TODO*/ },
-                            onNavigateToGear = { /*TODO*/ },
-                            onNavigateToLabels = { navController.navigate(route = Labels) },
-                            onNavigateToSettings = { navController.navigate(route = Settings) }
-                        )
-                    }
-                    composable<Labels> {
-                        LabelsScreen(
-                            mainViewModel = rollsModel,
-                            onNavigateUp = { navController.navigateUp() },
-                            onEditLabel = { label ->
-                                navController.navigate(route = LabelEdit(label?.id ?: -1))
-                            }
-                        )
-                    }
-                    composable<Settings> {
-                        SettingsScreen(
-                            themeViewModel = themeModel,
-                            settingsViewModel = settingsModel,
-                            mainViewModel = rollsModel,
-                            onNavigateUp = { navController.navigateUp() },
-                            onNavigateToLicense = { navController.navigate(route = License) },
-                            onNavigateToThirdPartyLicenses = {
-                                navController.navigate(route = ThirdPartyLicenses)
-                            }
-                        )
-                    }
-                    composable<License> {
-                        LicenseScreen(
-                            onNavigateUp = { navController.navigateUp() }
-                        )
-                    }
-                    composable<ThirdPartyLicenses> {
-                        ThirdPartyLicensesScreen(
-                            onNavigateUp = { navController.navigateUp() }
-                        )
-                    }
-                    dialog<LabelEdit> { backStackEntry ->
-                        val labelEdit = backStackEntry.toRoute<LabelEdit>()
-                        LabelEditScreen(
-                            labelId = labelEdit.labelId,
-                            onDismiss = { navController.navigateUp() },
-                            mainViewModel = rollsModel
-                        )
-                    }
-                }
-            }
-        }
+        setContent { App() }
     }
 
     override fun onStart() {
@@ -144,21 +61,3 @@ class MainActivity : ComponentActivity() {
         super.onStart()
     }
 }
-
-@Serializable
-object Main
-
-@Serializable
-object Labels
-
-@Serializable
-data class LabelEdit(val labelId: Long)
-
-@Serializable
-object Settings
-
-@Serializable
-object License
-
-@Serializable
-object ThirdPartyLicenses
