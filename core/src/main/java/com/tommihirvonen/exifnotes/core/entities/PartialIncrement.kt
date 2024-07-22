296b1ff8f0e985549875bef61f573b288ad4c265
Tommi Hirvonen <tommi.1.hirvonen@gmail.com>
2024-07-23 00:01:36 +0300

    [core] [PartialIncrement] description()

--- core/src/main/java/com/tommihirvonen/exifnotes/core/entities/PartialIncrement.kt
+++ core/src/main/java/com/tommihirvonen/exifnotes/core/entities/PartialIncrement.kt
@@ -18,9 +18,18 @@
 
 package com.tommihirvonen.exifnotes.core.entities
 
+import android.content.Context
+import com.tommihirvonen.exifnotes.core.R
+
 enum class PartialIncrement {
     THIRD, HALF;
+
     companion object {
         fun from(value: Int) = entries.firstOrNull { it.ordinal == value } ?: THIRD
+    }
+
+    fun description(context: Context) = when(this) {
+        THIRD -> context.getString(R.string.ThirdStop)
+        HALF -> context.getString(R.string.HalfStop)
     }
 }
